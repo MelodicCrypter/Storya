b6adfb6919aeb029d0cdc6992e2336ac44b0c733
Hugh Caluscusin <hugh@melodiccrypter.com>
2019-02-18 20:20:36 +0800

    added socket.io

--- server/server.js
+++ server/server.js
@@ -1,18 +1,35 @@
 // Library Modules
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
 
 // Local Modules
 
-// Instance of Express
+// Express, http, and socketIO
 const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+
 // Port
 const port = process.env.PORT || 3000;
-// Public Path
+
+// Setting for public routes
 const publicPath = path.join(__dirname, '../public');
-// Static directory
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
+// SOCKET.IO Event listeners
+io.on('connection', (socket) => {
+    console.log('New user connected');
+
+    socket.on('disconnect', () => {
+        console.log('User disconnected');
+    });
+});
+// io.on('disconnection')
+
+
+// App ready to run
+server.listen(port, () => {
     console.log(`Server up on port: ${port}`);
 });

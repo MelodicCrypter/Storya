aa114e0901f557b39570e558244cb372a422d902
Hugh Caluscusin <hugh@melodiccrypter.com>
2019-03-27 21:59:26 +0800

    modified validation.js

--- server/utils/validation.js
+++ server/utils/validation.js
@@ -1,5 +1,3 @@
-const isRealString = (str) => {
-    return typeof str === 'string' && str.trim().length > 0;
-};
+const isRealString = str => typeof str === 'string' && str.trim().length > 0;
 
 module.exports = { isRealString };
